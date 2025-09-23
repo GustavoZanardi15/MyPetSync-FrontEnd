4d3d095f147b87b2f0adaa148132e8cf8f7d8803
GustavoZanardi15 <ulianzanardig@gmail.com>
2025-09-23 15:19:03 -0300

    style(frontend): ajustar Hero.jsx e main.jsx

--- FrontEndWeb/src/main.jsx
+++ FrontEndWeb/src/main.jsx
@@ -3,9 +3,11 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import Header from "./components/shared/Header.jsx";
+import Hero from "./components/landing/Hero.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Header />
+    <Hero />
   </StrictMode>
 );
