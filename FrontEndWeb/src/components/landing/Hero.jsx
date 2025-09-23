e0eaf9d2fcab203b9049a9e1ed7ef962b727678b
GustavoZanardi15 <ulianzanardig@gmail.com>
2025-09-23 15:03:16 -0300

    style(frontend): atualizar logo e cor do header

---
+++ FrontEndWeb/src/components/landing/Hero.jsx
@@ -0,0 +1,17 @@
+const Hero = () => {
+  return (
+    <div className="bg-gray-100 flex items-center justify-center p-8">
+      <div className="container mx-auto flex flex-col md:flex-row items-center">
+        <div className="md:w-1/2 text-center md:text-left mb-8 md:mb-0">
+          <h1 className="text-4xl md:text-5xl font-bold text-teal-800 mb-4">
+            Bem vindo ao <br /> My Pet Sync!
+          </h1>
+          <p className="text-lg text-gray-700">
+            Centralize, gerencie e sincronize os cuidados do seu pet em um só
+            lugar.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
--- FrontEndWeb/src/components/shared/Header.jsx
+++ FrontEndWeb/src/components/shared/Header.jsx
@@ -1,4 +1,4 @@
-import logo from "../../assets/Logo.svg";
+import logo from "../../assets/Logo.png";
 
 const navItems = [
   { label: "Home", href: "#" },
@@ -9,15 +9,17 @@ const navItems = [
 
 const Header = () => {
   return (
-    <nav className="bg-teal-700 p-4 sticky top-0 z-50 py-3 backdrop-blur-lg border-b border-neutral-700/80">
+    <nav
+      className="p-4 sticky top-0 z-50 py-3 backdrop-blur-lg border-b border-neutral-700/80"
+      style={{ backgroundColor: "#058789" }}
+    >
       <div className="container px-4 mx-auto text-sm">
         <div className="flex justify-between items-center">
           <div className="flex items-center flex-shrink-0">
-            <img src={logo} alt="logo" className="h-12 w-auto" />
+            <img src={logo} alt="logo" className="h-14 w-auto" />
           </div>
-
-          <div className="hidden lg:flex justify-center space-x-12 items-center">
-            <ul className="flex space-x-8">
+          <div className="flex-1 flex-wrap flex justify-center">
+            <ul className="flex space-x-8 list-none">
               {navItems.map((item, index) => (
                 <li key={index}>
                   <a
@@ -30,8 +32,7 @@ const Header = () => {
               ))}
             </ul>
           </div>
-
-          <div className="hidden lg:flex justify-center items-center">
+          <div>
             <a
               href="#"
               className="bg-teal-900 text-white py-2 px-4 rounded-md hover:bg-teal-800"
