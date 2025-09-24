323011d8a32245827e7696fe99f666865f41f31c
GustavoZanardi15 <ulianzanardig@gmail.com>
2025-09-24 08:50:24 -0300

    feat(frontend): criar estrutura base da seção de funcionalidades

---
+++ FrontEndWeb/src/components/landing/Features.jsx
@@ -0,0 +1,18 @@
+const Features = () => {
+  return (
+    <div>
+      <div>
+        <div>
+          <h2>Aplicativo</h2>
+          <p>
+            O aplicativo foi desenvolvido especialmente para os donos de pets
+            que precisam de praticidade e organização no dia a dia.
+          </p>
+          <div></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Features;
