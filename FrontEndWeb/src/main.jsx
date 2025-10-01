3d3275f0ec8cf8e74fa4c06d7fa06b960ac46784
GustavoZanardi15 <ulianzanardig@gmail.com>
2025-10-01 15:11:36 -0300

    feat(routing): Configure React Router DOM v6 for client-side routing

--- FrontEndWeb/src/main.jsx
+++ FrontEndWeb/src/main.jsx
@@ -1,26 +1,23 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import Header from "./components/shared/Header.jsx";
-import Hero from "./components/landing/Hero.jsx";
-import About from "./components/landing/About.jsx";
-import Features from "./components/landing/Features.jsx";
-import Providers from "./components/landing/Providers.jsx";
-import Footer from "./components/shared/Footer.jsx";
-import Login from "./components/auth/Login.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import LandingPage from "./components/landing/LandingPage.jsx";
 import RegisterCompany from "./components/auth/RegisterCompany.jsx";
-import RegisterAutonomo from "./components/auth/RegisterAutonomo.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <LandingPage />,
+  },
+  {
+    path: "/registerCompany",
+    element: <RegisterCompany />,
+  },
+]);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Header />
-    <Hero />
-    <About />
-    <Features />
-    <Providers />
-    <Footer />
-    <Login />
-    <RegisterCompany />
-    <RegisterAutonomo />
+    <RouterProvider router={router} />
   </StrictMode>
 );
