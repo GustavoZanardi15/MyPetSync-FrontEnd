b5998a11ca8461cb06c47f5968f0cc2ae67b7f3e
GustavoZanardi15 <ulianzanardig@gmail.com>
2025-10-03 14:42:22 -0300

    chore(frontend): configurar rotas iniciais e importar HomePage

--- FrontEndWeb/src/main.jsx
+++ FrontEndWeb/src/main.jsx
@@ -6,8 +6,7 @@ import LandingPage from "./pages/LandingPage.jsx";
 import RegisterCompany from "./components/auth/RegisterCompany.jsx";
 import RegisterAutonomo from "./components/auth/RegisterAutonomo.jsx";
 import Login from "./components/auth/Login.jsx";
-import Header from "./components/common/Header.jsx";
-import Sidebar from "./components/common/Sidebar.jsx";
+import HomePage from "./pages/HomePage.jsx";
 
 const router = createBrowserRouter([
   {
@@ -26,12 +25,14 @@ const router = createBrowserRouter([
     path: "/login",
     element: <Login />,
   },
+  {
+    path: "/homePage",
+    element: <HomePage />,
+  },
 ]);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <RouterProvider router={router} />
-    <Header />
-    <Sidebar />
   </StrictMode>
 );
