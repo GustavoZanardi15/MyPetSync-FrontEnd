73c8c39c121f846f7f423518d12cece9a3b01034
GustavoZanardi15 <ulianzanardig@gmail.com>
2025-10-04 14:11:29 -0300

    feat(frontend): implementar componente de estatísticas do dashboard (DashboardStats)

--- FrontEndWeb/src/pages/HomePage.jsx
+++ FrontEndWeb/src/pages/HomePage.jsx
@@ -1,5 +1,6 @@
 import Header from "../components/common/Header";
 import Sidebar from "../components/common/Sidebar";
+import DashboardStats from "../components/home/DashboardStats";
 
 const HomePage = () => {
   return (
@@ -8,6 +9,7 @@ const HomePage = () => {
       <div className="flex flex-col flex-grow">
         <Header />
         <main className="p-8 bg-gray-100 flex-grow"></main>
+        <DashboardStats />
       </div>
     </div>
   );
