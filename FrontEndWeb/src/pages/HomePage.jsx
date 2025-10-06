f65d144a175bc4c17544f567937723254c96433f
GustavoZanardi15 <ulianzanardig@gmail.com>
2025-10-06 13:58:13 -0300

    refactor(frontend): integrar HomeSidePanel e reestruturar layout da HomePage para duas colunas

--- FrontEndWeb/src/pages/HomePage.jsx
+++ FrontEndWeb/src/pages/HomePage.jsx
@@ -3,6 +3,7 @@ import Sidebar from "../components/common/Sidebar";
 import DashboardStats from "../components/home/DashboardStats";
 import WelcomeBanner from "../components/home/WelcomeBanner";
 import NextAppointments from "../components/home/NextAppointments";
+import HomeSidePanel from "../components/home/HomeSidePanel";
 
 const HomePage = () => {
   return (
@@ -13,7 +14,14 @@ const HomePage = () => {
         <main className="px-8 pt-4 pb-8 bg-gray-100 flex-grow">
           <WelcomeBanner />
           <DashboardStats />
-          <NextAppointments />
+          <div className="flex gap-8 mt-8">
+            <div className="flex-grow">
+              <NextAppointments />
+            </div>
+            <div className="w-80">
+              <HomeSidePanel />
+            </div>
+          </div>
         </main>
       </div>
     </div>
