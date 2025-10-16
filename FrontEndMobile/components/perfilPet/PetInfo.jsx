68a77c2a642f3ab73e706fbeae31557fe0acedd5
AlissonKobayashi <alisson.kobayashi@gmail.com>
2025-10-16 14:36:31 -0300

    style(frontend): estilizar campos com informações do pet

--- FrontEndMobile/components/perfilPet/PetInfo.jsx
+++ FrontEndMobile/components/perfilPet/PetInfo.jsx
@@ -2,47 +2,58 @@ import React from "react";
 import { View, Text, StyleSheet } from "react-native";
 
 export default function PetInfo({ label, value }) {
-  return (
-    <View style={styles.infoFieldContainer}>
-      <Text style={styles.infoFieldLabel}>{label}</Text>
-      <Text
-        style={[
-          styles.infoFieldValue,
-          (value.trim() === "" || value.trim() === " ") && styles.infoFieldValueEmpty,
-        ]}
-      >
-        {value.trim() === "" || value.trim() === " " ? label : value}
-      </Text>
-    </View>
-  );
+
+    const isValueEmpty = value.trim() === "" || value.trim() === " ";
+
+    return (
+        <View style={styles.infoFieldContainer}>
+            <Text style={styles.infoFieldLabel}>{label}</Text>
+            <Text
+                style={[
+                    styles.infoFieldValue,
+                    isValueEmpty && styles.infoFieldValueEmpty,
+                ]}
+                numberOfLines={1}
+                ellipsizeMode="tail"
+            >
+                {isValueEmpty ? label : value}
+            </Text>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  infoFieldContainer: {
-    backgroundColor: "#FFFFFF",
-    borderRadius: 16,
-    paddingHorizontal: 20,
-    paddingVertical: 9.5,
-    marginBottom: 10,
-    shadowColor: "#000",
-    shadowOffset: { width: 0, height: 1 },
-    shadowOpacity: 0.05,
-    shadowRadius: 2,
-    elevation: 1,
-  },
-  infoFieldLabel: {
-    fontSize: 12,
-    color: "#A9A9A9",
-    fontWeight: "500",
-    marginBottom: 2,
-  },
-  infoFieldValue: {
-    fontSize: 16,
-    color: "#2F8B88",
-    fontWeight: "600",
-  },
-  infoFieldValueEmpty: {
-    color: "#A9A9A9",
-    fontWeight: "500",
-  },
+    infoFieldContainer: {
+        backgroundColor: "#FFFFFF",
+        borderRadius: 16,
+        paddingHorizontal: 20,
+        paddingVertical: 12,
+        marginBottom: 10,
+        flexDirection: "row",
+        alignItems: "center",
+        shadowColor: "#000",
+        shadowOffset: { width: 0, height: 1 },
+        shadowOpacity: 0.05,
+        shadowRadius: 2,
+        elevation: 1,
+    },
+    infoFieldLabel: {
+        fontSize: 12,
+        color: "#8E8E8E",
+        fontWeight: "regular",
+        width: 100,
+        textAlign: "left",
+    },
+    infoFieldValue: {
+        flex: 1,
+        fontSize: 15,
+        color: "#2F8B88",
+        fontWeight: "medium",
+        textAlign: "left",
+    },
+    infoFieldValueEmpty: {
+        color: "#2F8B88",
+        fontWeight: "600",
+        fontSize: 15,
+    }
 });
