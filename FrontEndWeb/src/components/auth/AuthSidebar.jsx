6ed9bcc1930ce3a72587aa922f357d4b19b0f8a0
GustavoZanardi15 <ulianzanardig@gmail.com>
2025-10-21 20:15:48 -0300

    style(frontend): refinar responsividade e padding do AuthSidebar

--- FrontEndWeb/src/components/auth/AuthSidebar.jsx
+++ FrontEndWeb/src/components/auth/AuthSidebar.jsx
@@ -1,27 +1,32 @@
 import Logo from "../../assets/Logo.png";
 
 const COLOR_TEAL = "#058789";
-const AuthSidebar = ({ children, widthClass = "lg:w-1/3" }) => {
+const AuthSidebar = ({
+  children,
+  widthClass = "md:w-2/5 lg:w-1/3 xl:w-1/4",
+}) => {
   return (
     <div
-      className={`w-full p-8 sm:p-12 md:p-32 flex flex-col ${widthClass}`}
+      className={`w-full h-full px-8 py-10 sm:px-12 md:px-16 md:py-16 lg:px-20 lg:py-24 flex flex-col ${widthClass} md:rounded-tr-[70px] md:rounded-br-[70px]`}
       style={{
         backgroundColor: COLOR_TEAL,
-        borderTopRightRadius: "70px",
-        borderBottomRightRadius: "70px",
       }}
     >
       <div className="flex flex-col items-center text-center h-full">
         <div className="w-full flex flex-col items-center">
           <div className="area-conteudo-topo w-full"></div>
-          <div className="mb-12 text-white">
-            <div className="mb-10">
+          <div className="mb-8 md:mb-12 text-white">
+            <div className="mb-8 md:mb-10">
               <p className="text-4xl text-[#003637] font-bold">Portal</p>
               <h1 className="text-4xl text-[#003637] font-bold">
                 Prestador de Serviço
               </h1>
             </div>
-            <img src={Logo} alt="Logo" className="w-44 h-48 mb-10 mx-auto" />
+            <img
+              src={Logo}
+              alt="Logo"
+              className="w-44 h-48 mb-4 md:mb-4 mx-auto"
+            />
           </div>
         </div>
         <div className="area-formulario w-full flex-grow">{children}</div>
