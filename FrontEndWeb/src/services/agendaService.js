b2542d7591c4423268688a4215ed46708b7c8f10
GustavoZanardi15 <ulianzanardig@gmail.com>
2025-11-03 16:46:02 -0300

    feat(agenda): criar services para agendamento e busca de pets

---
+++ FrontEndWeb/src/services/agendaService.js
@@ -0,0 +1,19 @@
+import api from "../utils/Api.jsx";
+
+export const getAppointments = async (startDate, endDate) => {
+  const response = await api.get("/appointments", {
+    params: {
+      from: startDate,
+      to: endDate,
+    },
+  });
+  return response.data.items || [];
+};
+
+export const createAppointment = async (appointmentData, providerId) => {
+  const response = await api.post(
+    `/providers/${providerId}/appointments`,
+    appointmentData
+  );
+  return response.data;
+};
