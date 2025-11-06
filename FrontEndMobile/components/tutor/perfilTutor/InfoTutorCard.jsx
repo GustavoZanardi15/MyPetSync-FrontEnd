a2a7efb7bccd100d86521ba8791b15236e87d6bb
AlissonKobayashi <alisson.kobayashi@gmail.com>
2025-11-06 19:07:36 -0300

    style(frontend): estlizando letras, tamanhos e icons da tela InfoTutorCard

--- FrontEndMobile/components/tutor/perfilTutor/InfoTutorCard.jsx
+++ FrontEndMobile/components/tutor/perfilTutor/InfoTutorCard.jsx
@@ -1,20 +1,29 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Image } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 
 export default function InfoTutorCard({ nome, email, telefone }) {
   return (
     <View style={styles.card}>
       <View style={styles.row}>
-        <Ionicons name="person-circle-outline" size={22} color="#2F7A67" />
+        <View style={styles.iconWrapper}>
+          <Image
+            source={require("../../../assets/images/home/NavBarPerfil.png")}
+            style={styles.iconImage}
+          />
+        </View>
         <Text style={styles.text}>{nome}</Text>
       </View>
       <View style={styles.row}>
-        <Ionicons name="mail-outline" size={20} color="#2F7A67" />
+        <View style={styles.iconWrapper}>
+          <Ionicons name="mail-outline" size={24} color="#2F7A67" />
+        </View>
         <Text style={styles.text}>{email}</Text>
       </View>
       <View style={styles.row}>
-        <Ionicons name="call-outline" size={20} color="#2F7A67" />
+        <View style={styles.iconWrapper}>
+          <Ionicons name="call-outline" size={24} color="#2F7A67" />
+        </View>
         <Text style={styles.text}>{telefone}</Text>
       </View>
     </View>
@@ -37,9 +46,22 @@ const styles = StyleSheet.create({
     alignItems: "center",
     marginVertical: 8,
   },
+  iconWrapper: {
+    width: 44,
+    height: 44,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 22,
+    backgroundColor: 'rgba(50,50,50,0.1)', 
+  },
+  iconImage: {
+    width: 24,
+    height: 24,
+    resizeMode: "contain",
+  },
   text: {
-    marginLeft: 10,
+    marginLeft: 12,
     fontSize: 15,
-    color: "#4D4D4D",
+    color: "#2F8B88",
   },
 });
