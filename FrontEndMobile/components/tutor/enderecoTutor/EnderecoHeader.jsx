52acf3e2ab8e201a3ff7da9d495453c47210c3a7
AlissonKobayashi <alisson.kobayashi@gmail.com>
2025-11-09 20:19:37 -0300

    refactor(frontend): refatorar seta de voltar para sessão anterior

--- FrontEndMobile/components/tutor/enderecoTutor/EnderecoHeader.jsx
+++ FrontEndMobile/components/tutor/enderecoTutor/EnderecoHeader.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { View, Text, Pressable, StyleSheet } from "react-native";
-import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { Ionicons } from "@expo/vector-icons";
 import { useRouter } from "expo-router";
 
 export default function EnderecoHeader() {
@@ -8,15 +8,8 @@ export default function EnderecoHeader() {
 
     return (
         <View style={styles.header}>
-            <Pressable
-                onPress={() =>
-                (router.canGoBack()
-                    ? router.back()
-                    : router.push("/screens/perfilTutorScreens/PerfilTutorScreen"))
-                }
-                style={styles.backBtn}
-            >
-                <MaterialCommunityIcons name="chevron-left" size={24} color="#2F8B88" />
+            <Pressable onPress={() => router.back()} style={styles.backButton}>
+                <Ionicons name="arrow-back" size={24} color="#2F8B88" />
             </Pressable>
             <Text style={styles.headerTitle}>Endereços</Text>
             <View style={{ width: 24 }} />
@@ -32,11 +25,8 @@ const styles = StyleSheet.create({
         marginHorizontal: 24,
         marginVertical: 16,
     },
-    backBtn: {
-        width: 24,
-        height: 24,
-        justifyContent: "center",
-        alignItems: "center"
+    backButton: {
+        padding: 5
     },
     headerTitle: {
         fontSize: 20,
