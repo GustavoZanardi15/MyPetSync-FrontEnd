63069819239a31f63e025f6a202cdeecaf5ce90b
João Renzetti <jaorenzetti@outlook.com>
2025-12-03 14:14:49 -0300

    fix: import authcontext no chatlistpage

--- FrontEndWeb/src/pages/chat/ChatListPage.jsx
+++ FrontEndWeb/src/pages/chat/ChatListPage.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import api from "../../utils/Api";
-import { useAuth } from "src/context/AuthContext";
+import { useAuth } from "../../context/AuthContext";
 
 function ChatListPage() {
   const [rooms, setRooms] = useState([]);
